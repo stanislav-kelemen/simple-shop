84d57ebdc55ca019d692c07da7cd68948f131e92
KLMN <oxxx1stas@gmail.com>
2020-09-20 18:11:18 +0300

    build

--- README.md
+++
@@ -1,3 +0,0 @@
-# simple-shop
-
-simple shop using redux
---
+++ precache-manifest.436e35c974195faa891306b63f261005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ee5909ad879b4dbcda7254060a21c1",
+    "url": "/simple-shop/index.html"
+  },
+  {
+    "revision": "fa507258cefb5946ff75",
+    "url": "/simple-shop/static/css/main.bd223fbf.chunk.css"
+  },
+  {
+    "revision": "99c4e4bd62605e5c6f18",
+    "url": "/simple-shop/static/js/2.ee742f82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-shop/static/js/2.ee742f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa507258cefb5946ff75",
+    "url": "/simple-shop/static/js/main.b81af52b.chunk.js"
+  },
+  {
+    "revision": "a340566a1d5cee30f720",
+    "url": "/simple-shop/static/js/runtime-main.c704a670.js"
+  }
+]);
